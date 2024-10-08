6967b130164c53b1b41a3fdff47a6ba7a57e07df
NNguyenK18 <nhatnguyen211278@gmail.com>
2024-10-08 13:37:12 +0700

    add btn5

--- Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c4670f09d48ab9e51e6c7eb4009b52ecd4844d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b463c09be2c4565c32f9e370517e17f556b6cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
