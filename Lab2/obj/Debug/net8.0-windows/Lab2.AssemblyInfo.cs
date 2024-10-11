74e3dac3d4a288954ff8c9e9347adfc512ea4e9d
TrThuyTien <tranthithuytien0615@gmail.com>
2024-10-11 22:52:10 +0700

    Update BT2 lan 1

--- Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2df103dc81cc91076a0faf47f61491359c34ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00edb55e054afa017dd6c43b29251307e281bb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
