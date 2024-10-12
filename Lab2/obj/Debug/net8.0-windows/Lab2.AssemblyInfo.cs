591d7ff1976944c428b83fbf5d436ea5a97dc316
TrThuyTien <tranthithuytien0615@gmail.com>
2024-10-12 23:00:52 +0700

    ktra lan 1

--- Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00edb55e054afa017dd6c43b29251307e281bb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9cd0a2e0a1a98485caeb7f62b07a22f56dcfb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
