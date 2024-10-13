da6147ac9f20c39610048186c1fa6436e4556ee0
TrThuyTien <tranthithuytien0615@gmail.com>
2024-10-13 09:52:23 +0700

    Update code BT5

--- Lab2/BT5.cs
+++ Lab2/BT5.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Lab2
 {
@@ -15,6 +18,178 @@ namespace Lab2
         public BT5()
         {
             InitializeComponent();
+        }
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        private ImageList imageList;
+        private void btn_Browse_Click(object sender, EventArgs e)
+        {
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.ShowNewFolderButton = true;
+            fbd.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string selectedPath = fbd.SelectedPath;
+                tb_Path.Text = selectedPath;
+                // Khởi tạo ImageList và thêm hình ảnh
+                /*imageList = new ImageList();
+
+                // Thay thế đường dẫn tới hình ảnh bạn đã tải lên
+                imageList.Images.Add("folder", Image.FromFile("C:\\path_to_your_folder_icon.png")); // Biểu tượng thư mục
+                imageList.Images.Add("file", Image.FromFile("C:\\path_to_your_file_icon.png")); // Biểu tượng tệp
+
+                listView_InforFile.SmallImageList = imageList; // Gán ImageList cho ListView
+
+                Controls.Add(listView_InforFile);*/
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(selectedPath);
+                DirectoryInfo[] folderArr = directoryInfo.GetDirectories();
+                FileInfo[] fileArr = directoryInfo.GetFiles();
+                foreach (DirectoryInfo folder in folderArr)
+                {
+                    ListViewItem item = new ListViewItem(folder.Name, "folder");
+                    item.SubItems.Add(folder.LastWriteTime.ToString());
+                    item.SubItems.Add("File Folder");
+                    item.SubItems.Add("");
+                    item.SubItems.Add("");
+                    listView_InforFile.Items.Add(item);
+                }
+                foreach (FileInfo file in fileArr)
+                {
+                    ListViewItem item = new ListViewItem(file.Name.Substring(0, file.Name.LastIndexOf('.')), "file");
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+                    item.SubItems.Add(file.Extension.Substring(1).ToUpper());
+                    double sizeInKb = file.Length / 1024.0;
+                    string fileSize;
+                    if (sizeInKb < 1024)
+                    {
+                        fileSize = sizeInKb.ToString("F2") + " KB";
+                    }
+                    else
+                    {
+                        fileSize = (sizeInKb / 1024.0).ToString("F2") + " MB";
+                    }
+                    item.SubItems.Add(fileSize);
+                    item.SubItems.Add("");
+                    listView_InforFile.Items.Add(item);
+
+                }
+            }
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            string selectedPath = fbd.SelectedPath;
+            MessageBox.Show(fbd.SelectedPath.ToString());
+            int index = selectedPath.LastIndexOf('\\');
+            if (index != -1)
+            {
+
+                selectedPath = selectedPath.Substring(0, index);
+                if (selectedPath.EndsWith(":"))
+                {
+                    selectedPath = selectedPath + "\\";
+                }
+                tb_Path.Text = selectedPath;
+                fbd.SelectedPath = selectedPath;
+                listView_InforFile.Items.Clear();
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(selectedPath);
+                DirectoryInfo[] folderArr = directoryInfo.GetDirectories();
+                FileInfo[] fileArr = directoryInfo.GetFiles();
+                foreach (DirectoryInfo folder in folderArr)
+                {
+                    ListViewItem item = new ListViewItem(folder.Name, "folder");
+                    item.SubItems.Add(folder.LastWriteTime.ToString());
+                    item.SubItems.Add("File Folder");
+                    item.SubItems.Add("");
+                    item.SubItems.Add("");
+                    listView_InforFile.Items.Add(item);
+                }
+                foreach (FileInfo file in fileArr)
+                {
+                    ListViewItem item = new ListViewItem(file.Name.Substring(0, file.Name.LastIndexOf('.')), "file");
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+                    item.SubItems.Add(file.Extension.Substring(1).ToUpper());
+                    double sizeInKb = file.Length / 1024.0;
+                    string fileSize;
+                    if (sizeInKb < 1024)
+                    {
+                        fileSize = sizeInKb.ToString("F2") + " KB";
+                    }
+                    else
+                    {
+                        fileSize = (sizeInKb / 1024.0).ToString("F2") + " MB";
+                    }
+                    item.SubItems.Add(fileSize);
+                    item.SubItems.Add("");
+                    listView_InforFile.Items.Add(item);
+
+                }
+                if (selectedPath.EndsWith(":\\"))
+                {
+                    selectedPath = selectedPath.Substring(0, index - 1);
+                }
+            }
+            else
+            {
+                selectedPath = String.Empty;
+            }
+
+        }
+        private void tb_Path_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string rootPath = fbd.SelectedPath;
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                string selectedPath = tb_Path.Text;
+                if (Directory.Exists(selectedPath))
+                {
+                    tb_Path.Text = selectedPath;
+                    fbd.SelectedPath = selectedPath;
+                    listView_InforFile.Items.Clear();
+
+                    DirectoryInfo directoryInfo = new DirectoryInfo(selectedPath);
+                    DirectoryInfo[] folderArr = directoryInfo.GetDirectories();
+                    FileInfo[] fileArr = directoryInfo.GetFiles();
+                    foreach (DirectoryInfo folder in folderArr)
+                    {
+                        ListViewItem item = new ListViewItem(folder.Name, "folder");
+                        item.SubItems.Add(folder.LastWriteTime.ToString());
+                        item.SubItems.Add("File Folder");
+                        item.SubItems.Add("");
+                        item.SubItems.Add("");
+                        listView_InforFile.Items.Add(item);
+                    }
+                    foreach (FileInfo file in fileArr)
+                    {
+                        ListViewItem item = new ListViewItem(file.Name.Substring(0, file.Name.LastIndexOf('.')), "file");
+                        item.SubItems.Add(file.LastWriteTime.ToString());
+                        item.SubItems.Add(file.Extension.Substring(1).ToUpper());
+                        double sizeInKb = file.Length / 1024.0;
+                        string fileSize;
+                        if (sizeInKb < 1024)
+                        {
+                            fileSize = sizeInKb.ToString("F2") + " KB";
+                        }
+                        else
+                        {
+                            fileSize = (sizeInKb / 1024.0).ToString("F2") + " MB";
+                        }
+                        item.SubItems.Add(fileSize);
+                        item.SubItems.Add("");
+                        listView_InforFile.Items.Add(item);
+
+                    }
+                    fbd.SelectedPath = selectedPath;
+                }
+                else
+                {
+                    MessageBox.Show("Thư mục không tồn tại.");
+                    fbd.SelectedPath = rootPath;
+                    tb_Path.Text = rootPath;
+                }
+            }
         }
     }
 }
--- Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf722f4b35c333511cac356b2519c32da34cf11a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd0313b22b71d26b354c0b25a9c50f099581a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
