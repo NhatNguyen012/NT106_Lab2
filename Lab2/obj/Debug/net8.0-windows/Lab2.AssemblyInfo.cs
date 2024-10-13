4bdad17450c39530efee5911320b20180a524676
NNguyenK18 <nhatnguyen211278@gmail.com>
2024-10-13 12:06:49 +0700

    BT1

--- Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd0313b22b71d26b354c0b25a9c50f099581a89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d08e490b414849b51242d23b67d00203ab25b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
